991a7b765bc0ab883ee3b4ac37b2a4ca06806298
Jodionio Muachifi <jodionisiomuachifi@ua.pt>
2022-10-03 19:28:18 +0100

    wav_cmp and wav_quant_v1

--- wav_hist.h
+++ wav_hist.h
@@ -6,16 +6,16 @@
 #include <map>
 #include <sndfile.hh>
 
-class WAVHist 
+class WAVHist
 {
-  private:
+private:
 	std::vector<std::map<short, size_t>> counts;
 	bool bStereo;
 
-  public:
-	WAVHist(const SndfileHandle& sfh) : bStereo(false)
+public:
+	WAVHist(const SndfileHandle &sfh) : bStereo(false)
 	{
-		if(sfh.channels() == 2)
+		if (sfh.channels() == 2)
 		{
 			counts.resize(sfh.channels() + 2);
 			bStereo = true;
@@ -26,14 +26,14 @@ class WAVHist
 		}
 	}
 
-	void update(const std::vector<short>& samples) 
+	void update(const std::vector<short> &samples)
 	{
-		size_t n { };
-		for(auto s : samples)
+		size_t n{};
+		for (auto s : samples)
 		{
-			if(bStereo && n % 2 == 1)
+			if (bStereo && n % 2 == 1)
 			{
-				short left = samples[n-1];
+				short left = samples[n - 1];
 				short right = samples[n];
 
 				counts[2][(left + right) / 2]++; // MID
@@ -44,12 +44,11 @@ class WAVHist
 		}
 	}
 
-	void dump(const size_t channel) const 
+	void dump(const size_t channel) const
 	{
-		for(auto [value, counter] : counts[channel])
+		for (auto [value, counter] : counts[channel])
 			std::cout << value << '\t' << counter << '\n';
 	}
 };
 
 #endif
-

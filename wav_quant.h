ed7587af13677058a27e697a145987dc22a86e3e
Immodicus <53706492+Immodicus@users.noreply.github.com>
2022-10-10 15:47:34 +0100

    quantization improvements

--- wav_quant.h
+++ wav_quant.h
@@ -15,22 +15,31 @@ class WAVQuant
 
         const uint64_t BufferSize = 256000;
 
-        int16_t Quantize(short s, uint8_t bBits)
+        int16_t Quantize(int16_t s, uint8_t bBits)
         {
             if(bBits == 0 || bBits > 15) throw new std::runtime_error("Invalid number of bits");
 
-            constexpr uint32_t srcNumBits = sizeof(short) * 8;
+            uint32_t srcNumBits = sizeof(int16_t) * 8;
             const uint32_t destNumBits = bBits;
-
+            
             const uint32_t uBitDiff = srcNumBits - destNumBits;
 
             const uint16_t uDiff = pow(2, uBitDiff);
+            const uint16_t uHalf = uDiff / 2;
+            const int16_t uMax = (((int16_t)(pow(2, destNumBits))) / 2) - 1;
 
             int16_t result = s / uDiff;
 
-            if(s < 0 && s % uDiff != 0)
+            if(result < uMax)
             {
-                result--;
+                if(s > 0)
+                {
+                    if(abs(s % uDiff) >= uHalf) result++;
+                }
+                else
+                {
+                    if(abs(s % uDiff) > uHalf) result--;
+                }
             }
 
             return result;
